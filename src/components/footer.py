26988d6348bcfc05e8a3887d106c74255af01d42
riya <kandwalriya121@gmail.com>
2026-07-06 10:20:56 +0530

    homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+
+def footer_home():
+   
+
+
+    st.markdown("""
+        <div style="
+        margin-top:2rem;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        gap:8px;
+        ">
+
+       <p style="
+       color:white;
+       font-family:Outfit,sans-serif;
+       font-size:20px;
+       font-weight:600;
+       margin-right:0px;
+       width:100%;
+       >
+       
+       
+       Created With ❤️ by
+       </p>
+
+       <img src="https://i.ibb.co/21mZ22XR/Chat-GPT-Image-Jul-6-2026-at-09-36-33-AM-4.png"
+       style="height:50px;
+       margin-top:5px;">
+
+       </div>
+    """, unsafe_allow_html=True)
+    
---
+++ src/components/header.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def header_home():
+
+   
+    st.markdown("""
+                <div style='display:flex; flex-direction:column; align-items:center; justify-content:center; margin-bottom:30px; margin-top:30px;'>
+               <img src='https://i.ibb.co/YTYGn5qV/logo.png' style='height:100px;'>
+                <h1 style='text-align:center; color:#e0e3ff'>SNAP <br>CLASS</h1>
+
+                <div>
+                
+                """,unsafe_allow_html=True)
+    
